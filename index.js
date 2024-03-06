96e668f36cc0094b3de1667bb77ebe468212fa0e
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-03-06 11:51:47 -0500

    Practiced three different methods to complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+  return "This one is on me!";
+}
+  else if (distance > 400 && distance <= 2000){
+  return "That will be twenty bucks.";  
+}
+  else if (distance >2000 && distance <= 2500){
+  return "I will gladly take your thirty bucks.";  // Write your code here!
+}
+  else if (distance > 2500){
+  return "No can do."  
+}
+}
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+  switch (generous){
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";    
+  }  
 }
